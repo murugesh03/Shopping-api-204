dafdb95d817ca495f3d7e46d8384e01f7a83db22
Murugesh Padmanaban <murugeshvj@gmail.com>
2026-04-04 06:15:26 +0000

    typo error

--- src/middleware/upload.middleware.js
+++ src/middleware/upload.middleware.js
@@ -1,6 +1,6 @@
 const multer = require("multer");
 const path = require("path");
-const fs = reqire("fs");
+const fs = require("fs");
 const uploadPath = path.join(__dirname,'../uploads')
 if(!fs.existsSync(uploadPath)){
  fs.mkdirSync(uploadPath, { recursive: true });
